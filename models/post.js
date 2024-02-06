f683b34e122f19963ebdea9552adeefde872f428
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-06 15:42:32 -0500

    Add model for post records

---
+++ models/post.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    minLength: 1,
+    maxLength: 200,
+    required: true,
+  },
+  text: {
+    type: String,
+    minLength: 1,
+    required: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  published: {
+    type: Boolean,
+    required: true,
+  },
+  comments: {
+    type: [Schema.Types.ObjectId],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
