c334c893cca46a4cbdc03769a32f3d09addd9edc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-06 17:25:34 -0500

    Assign routes using user controller

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,15 @@ const userController = require("../controllers/userController");
 router.post("/", userController.create_user);
 
 // Update a user
+router.put("/:id", userController.update_user);
+
 // Delete a user
+router.delete("/:id", userController.delete_user);
+
 // Log in to user account
+router.post("/login", userController.login);
+
 // Log out of user account
+router.post("/logout", userController.logout);
 
 module.exports = router;
