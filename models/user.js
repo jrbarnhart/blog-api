44fc3aba9d2acb6422f4d3ff1697e182195eac0a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-06 15:26:47 -0500

    Add model for user records

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    minLength: 1,
+    maxLength: 200,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: "Invalid email address format",
+    },
+  },
+  display_name: {
+    type: String,
+    minLength: 1,
+    maxLength: 200,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  access: {
+    type: String,
+    enum: {
+      values: ["basic", "admin"],
+      message: "enum validator failed for path `{PATH}` with value `{VALUE}`",
+    },
+    required: true,
+  },
+});
