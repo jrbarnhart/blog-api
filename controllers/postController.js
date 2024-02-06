c5e117497d43e9569f68187b5fd0c20410aa8759
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-06 16:41:07 -0500

    Add required modules

---
+++ controllers/postController.js
@@ -0,0 +1,5 @@
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const { decode } = require("html-entities");
+
+const Post = require("../models/post");
