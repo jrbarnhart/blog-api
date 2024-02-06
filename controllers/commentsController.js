480448682dd635cca55c939ecf85b1655718fe6e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-06 17:14:31 -0500

    Add placeholder route handlers

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,3 +1,30 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const { decode } = require("html-entities");
+
+const Comment = require("../models/comment");
+
+// Create a user
+exports.create_user = (req, res) => {
+  res.send("Create a user NYI");
+};
+
+// Update a user
+exports.update_user = (req, res) => {
+  res.send("Update a user NYI");
+};
+
+// Delete a user
+exports.delete_user = (req, res) => {
+  res.send("Delete a user NYI");
+};
+
+// Log in to user account
+exports.login = (req, res) => {
+  res.send("Login NYI");
+};
+
+// Log out of user account
+exports.logout = (req, res) => {
+  res.send("Logout NYI");
+};
