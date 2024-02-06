4c01c97772f1e579c47c30a942ce69b841ff01db
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-06 16:51:25 -0500

    Add placeholder routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,3 +3,23 @@ const { body, validationResult } = require("express-validator");
 const { decode } = require("html-entities");
 
 const Post = require("../models/post");
+
+// Get a post
+exports.get_post = (req, res) => {
+  res.send("Post lookup NYI");
+};
+
+// Update a post
+exports.update_post = (req, res) => {
+  res.send("Post update NYI");
+};
+
+// Create a post
+exports.create_post = (req, res) => {
+  res.send("Post create NYI");
+};
+
+// Delete a post
+exports.delete_post = (req, res) => {
+  res.send("Post delete NYI");
+};
