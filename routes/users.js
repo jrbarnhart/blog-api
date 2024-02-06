d32bbc9a4071f6e6d8eed2599bc915bbbb348930
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-06 17:22:46 -0500

    Add route for creating user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+const userController = require("../controllers/userController");
+
+// Create a user
+router.post("/", userController.create_user);
+
+// Update a user
+// Delete a user
+// Log in to user account
+// Log out of user account
 
 module.exports = router;
