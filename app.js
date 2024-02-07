3195194998fd3f3eb6dbe817b8d4e371ba46cc83
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-07 17:40:24 -0500

    Add error handler

--- app.js
+++ app.js
@@ -26,4 +26,23 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  const errStatus = err.status || 500;
+  const errMessage = err.message || "An error occurred";
+
+  res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMessage,
+    stack: process.env.NODE_ENV === "development" ? err.stack : {},
+  });
+});
+
 module.exports = app;
