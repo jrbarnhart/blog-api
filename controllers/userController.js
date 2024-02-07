1f56650c220380d1bc72b0d58313dd045a664bff
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-07 15:20:49 -0500

    San/val display name

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,44 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
 // Create a user
 exports.create_user = asyncHandler(async (req, res, next) => {
-  body("username")
-    .trim()
-    .escape()
-    .exists({ values: "falsy" })
-    .withMessage("Username requrired")
-    .isLength({ min: 3, max: 200 })
-    .withMessage("Username must be between 3-200 characters")
-    .isEmail()
-    .withMessage("Username must be an email")
-    .custom(async (value, { req }) => {
-      const existingUser = await User.findOne({ username: value });
-      if (existingUser) throw new Error("Username/email already in use");
-    })
-    .withMessage("Username/email already in use"),
-    body("display_name");
-  body("password");
-  body("confirm_password");
-  body("is_admin");
-  body("admin_password");
+  [
+    body("username")
+      .isString()
+      .withMessage("Username must be a string")
+      .trim()
+      .escape()
+      .exists({ values: "falsy" })
+      .withMessage("Username requrired")
+      .isLength({ min: 3, max: 200 })
+      .withMessage("Username must be between 3-200 characters")
+      .isEmail()
+      .withMessage("Username must be an email")
+      .normalizeEmail()
+      .custom(async (value, { req }) => {
+        const existingUser = await User.findOne({ username: value });
+        if (existingUser) throw new Error("Username/email already in use");
+      })
+      .withMessage("Username/email already in use"),
+    body("display_name")
+      .isString()
+      .withMessage("Display name must be a string")
+      .trim()
+      .escape()
+      .exists({ values: "falsy" })
+      .withMessage("Display name required")
+      .isLength({ min: 1, max: 25 })
+      .withMessage("Display name must be between 1-25 characters"),
+    body("password"),
+    body("confirm_password"),
+    body("is_admin"),
+    body("admin_password"),
+  ];
 });
 
 // Update a user
