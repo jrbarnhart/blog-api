f8818d5ac312bce6aae1edfd1438900ce85862a7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-08 14:07:23 -0500

    Add middleware script for verifying jwt

---
+++ scripts/verifyToken.js
@@ -0,0 +1,17 @@
+// Format of token:
+// Authorization: Bearer <access_token>
+
+const verifyToken = (req, res, next) => {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    // Verify token here
+  } else {
+    // Forbidden
+    res.status(403).json({
+      success: false,
+      status: 403,
+      message: "Access forbidden",
+    });
+  }
+};
