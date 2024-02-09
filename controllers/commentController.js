aaceadf2787006ef173d81efa65ba1fd08c9e6c7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-09 17:45:25 -0500

    Add handler creating a comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,18 +3,46 @@ const { body, validationResult } = require("express-validator");
 const { decode } = require("html-entities");
 
 const Comment = require("../models/comment");
+const Post = require("../models/post");
 const { checkTokenRequired, validateToken } = require("../scripts/checkToken");
 
 // Create a comment
 exports.create_comment = [
   checkTokenRequired,
   validateToken,
-  (req, res) => {
-    res.json({
-      message: "Post created NYI",
-      authData: res.authData,
-    });
-  },
+
+  body("text")
+    .isString()
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Text required"),
+
+  asyncHandler(async (req, res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+      res.status(403).json({
+        success: false,
+        status: 403,
+        text: req.body.text,
+        errors: validationErrors.array(),
+      });
+    } else {
+      const newComment = new Comment({
+        text: req.body.text,
+        author: res.authData.user._id,
+        date: new Date(),
+        post: req.params.postId,
+      });
+
+      await newComment.save();
+      res.json({
+        success: true,
+        newComment,
+      });
+    }
+  }),
 ];
 
 // Get comments in a post
