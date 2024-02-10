59d7275717d01eff8f6baf5f54d426c8f2fe6bad
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-10 17:54:00 -0500

    Add new body validation error middleware

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,6 +5,7 @@ const { decode } = require("html-entities");
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 const { checkTokenRequired, validateToken } = require("../scripts/checkToken");
+const handleValidationErrors = require("../scripts/handleValidationErrors");
 
 // Create a comment
 exports.create_comment = [
@@ -18,38 +19,29 @@ exports.create_comment = [
     .isLength({ min: 1 })
     .withMessage("Text required"),
 
-  asyncHandler(async (req, res, next) => {
-    const validationErrors = validationResult(req);
+  handleValidationErrors(["text"]),
 
-    if (!validationErrors.isEmpty()) {
-      res.status(403).json({
+  asyncHandler(async (req, res, next) => {
+    const postToCommentOn = await Post.findById(req.params.postId).exec();
+    if (!postToCommentOn) {
+      res.status(404).json({
         success: false,
-        status: 403,
-        text: req.body.text,
-        errors: validationErrors.array(),
+        status: 404,
+        message: "Resource not found",
       });
     } else {
-      const postToCommentOn = await Post.findById(req.params.postId).exec();
-      if (!postToCommentOn) {
-        res.status(404).json({
-          success: false,
-          status: 404,
-          message: "Resource not found",
-        });
-      } else {
-        const newComment = new Comment({
-          text: req.body.text,
-          author: res.authData.user._id,
-          date: new Date(),
-          post: req.params.postId,
-        });
+      const newComment = new Comment({
+        text: req.body.text,
+        author: res.authData.user._id,
+        date: new Date(),
+        post: req.params.postId,
+      });
 
-        await newComment.save();
-        res.json({
-          success: true,
-          newComment,
-        });
-      }
+      await newComment.save();
+      res.json({
+        success: true,
+        newComment,
+      });
     }
   }),
 ];
@@ -83,47 +75,36 @@ exports.update_comment = [
     .isLength({ min: 1 })
     .withMessage("Text required"),
 
-  asyncHandler(async (req, res, next) => {
-    const validationErrors = validationResult(req);
+  handleValidationErrors(["text"]),
 
-    if (!validationErrors.isEmpty()) {
+  asyncHandler(async (req, res, next) => {
+    // Check that comment exists
+    const updatedComment = await Comment.findById(req.params.commentId).exec();
+    if (!updatedComment) {
+      res.status(404).json({
+        success: false,
+        status: 404,
+        message: "Resource not found",
+      });
+      // Check if author or admin
+    } else if (
+      !(
+        res.authData.user._id === updatedComment.author.toString() ||
+        res.authData.user.access === "admin"
+      )
+    ) {
       res.status(403).json({
         success: false,
         status: 403,
-        text: req.body.text,
-        errors: validationErrors.array(),
+        message: "Access forbidden",
       });
     } else {
-      // Check that comment exists
-      const updatedComment = await Comment.findById(
-        req.params.commentId
-      ).exec();
-      if (!updatedComment) {
-        res.status(404).json({
-          success: false,
-          status: 404,
-          message: "Resource not found",
-        });
-        // Check if author or admin
-      } else if (
-        !(
-          res.authData.user._id === updatedComment.author.toString() ||
-          res.authData.user.access === "admin"
-        )
-      ) {
-        res.status(403).json({
-          success: false,
-          status: 403,
-          message: "Access forbidden",
-        });
-      } else {
-        updatedComment.text = req.body.text;
-        await updatedComment.save();
-        res.json({
-          success: true,
-          updatedComment,
-        });
-      }
+      updatedComment.text = req.body.text;
+      await updatedComment.save();
+      res.json({
+        success: true,
+        updatedComment,
+      });
     }
   }),
 ];
